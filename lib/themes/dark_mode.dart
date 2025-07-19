a60e827fcca11ef1a5080f3abe45d2e89850c5a4
Weerathunga T K T N <135198957+Nimdinu-Weerathunga@users.noreply.github.com>
2025-07-19 23:37:11 +0530

    creates themes

---
+++ lib/themes/dark_mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+  colorScheme: ColorScheme.dark(
+    background: const Color.fromARGB(255, 69, 64, 64),
+    primary: const Color.fromARGB(255, 41, 37, 37),
+    secondary: Colors.grey.shade300,
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade700,
+  ),
+);
---
+++ lib/themes/light_mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Colors.grey.shade300,
+    primary: Colors.grey.shade300,
+    secondary: Colors.grey.shade300,
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade700,
+  ),
+);
