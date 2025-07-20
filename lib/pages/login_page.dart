3c548132a5daf57458016b58c8e1c67e6e6e3650
Weerathunga T K T N <135198957+Nimdinu-Weerathunga@users.noreply.github.com>
2025-07-20 23:59:03 +0530

    create login page😒

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app_w_backend/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  // text editing controller
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          //logo
+          Icon(
+            Icons.lock_open_rounded,
+            size: 100,
+            color: Theme.of(context).colorScheme.inversePrimary,
+          ),
+          SizedBox(height: 25),
+          //msg,app slogan
+          Text(
+            "Food Delivery App",
+            style: TextStyle(
+              fontSize: 16,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          ),
+          SizedBox(height: 25),
+
+          //email textfield
+          MyTextfield(
+            controller: emailController,
+            hintText: "Email",
+            obscureText: false,
+          ),
+          //sign in button
+
+          //not a member register now
+        ],
+      ),
+    );
   }
 }
