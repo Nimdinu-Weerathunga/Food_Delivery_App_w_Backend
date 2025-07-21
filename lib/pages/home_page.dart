3c3cf5010517e5fb3973f5f75dccc3c858e88fa4
Weerathunga T K T N <135198957+Nimdinu-Weerathunga@users.noreply.github.com>
2025-07-21 23:59:24 +0530

    Create menu file

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app_w_backend/components/my_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,7 +8,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("HomePage")),
-      drawer: Drawer(),
+      drawer: MyDrawer(),
     );
   }
 }
